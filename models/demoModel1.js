4a6573599acde4fca77b39482eb196d367375b23
taufik69 <taufik35-2915@diu.edu.bd>
2023-04-11 21:39:53 +0600

    xx

--- models/demoModel1.js
+++ models/demoModel1.js
@@ -3,14 +3,9 @@ const { Schema } = mongoose;
 
 const demodel = new Schema({
   title: String,
-  logos: [
-    { src: String },
-    // { src: String },
-    // { src: String },
-    // { src: String },
-    // { src: String },
-  ],
-  updated: { type: Date, default: Date.now },
+  logos: [{ src: String }],
+
+  // updated: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model("title", demodel);
